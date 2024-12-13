7675305596be16b3342fbce6caad75979e2048c3
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2024-12-13 22:54:53 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -7,13 +7,8 @@ using namespace std;
 
 int solution(vector<int> num_list) {
     int answer = 0;
-    int multi = 1;
     
-    for (int i = 0 ; i < num_list.size(); i++) {
-        multi *= num_list[i];
-    }
-    
-    answer = multi < pow(accumulate(num_list.begin(), num_list.end(), 0), 2) ? 1 : 0;
+    answer = accumulate(num_list.begin(), num_list.end(), 1, multiplies<int>()) < pow(accumulate(num_list.begin(), num_list.end(), 0), 2) ? 1 : 0;
     
     return answer;
 }
