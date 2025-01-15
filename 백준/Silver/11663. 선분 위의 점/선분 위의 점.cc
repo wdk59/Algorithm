be95d1b77f283bad24e2fc5cee598827a18ebf1c
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-01-15 14:48:13 +0900

    [Silver III] Title: 선분 위의 점, Time: 68 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.cc
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.cc
@@ -3,36 +3,13 @@
 #include <algorithm>	// sort
 using namespace std;
 
-vector<int> dots;
-
-int search(int goal, bool isStart) {
-	int p = 0;
-	int left = 0, right = dots.size() - 1;
-
-	while (left <= right) {
-		int mid = (left + right) / 2;
-
-		if (dots[mid] < goal) {
-			p = mid + 1;
-			left = mid + 1;
-		}
-		else if (dots[mid] > goal) {
-			right = mid - 1;
-		}
-		else {
-			return isStart ? mid : mid + 1;
-		}
-	}
-
-	return p;
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	int n = 0, m = 0;
+	vector<int> dots;
 
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
@@ -46,7 +23,7 @@ int main() {
 		int start = 0, end = 0;
 		cin >> start >> end;
 	
-		cout << search(end, false) - search(start, true) << "\n";
+		cout <<  upper_bound(dots.begin(), dots.end(), end) - lower_bound(dots.begin(), dots.end(), start) << "\n";
 	}
 
 	return 0;
