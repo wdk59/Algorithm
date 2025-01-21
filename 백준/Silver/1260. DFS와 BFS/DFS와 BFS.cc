3818807ee5dba24ff877c3ede0a8627b73b60cc5
wdk59 <93441225+wdk59@users.noreply.github.com>
2025-01-22 05:55:12 +0900

    Update DFS와 BFS.cc

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -2,48 +2,62 @@
 #include <queue>
 using namespace std;
 
-int n = 0, m = 0, v = 0;
-int graph[1001][1001];	// 간선 저장. 초기화: 0
-bool visited[1001];		// 방문했는가. 초기화: 0
+int N = 0, M = 0, V = 0;
+int edges[1001][1001];	// 간선
+bool visited[1001];			// 노드 방문 여부
 
 void DFS(int v) {
+	// 노드 방문
 	cout << v << " ";
-	visited[v] = 1;	// 방문했으면 1
-	for (int i = 1; i <= n; i++) {
-		if (visited[i] == 1 || graph[v][i] == 0)
+	visited[v] = true;	// 방문: true
+	// 방문한 노드와 연결된 노드 탐색
+	for (int i = 1; i <= N; i++) {
+		// 간선이 연결되어 있지 않거나 이미 방문한 노드는 건너뛰기
+		if (edges[v][i] == 0 || visited[i])
 			continue;
+
 		DFS(i);
 	}
 }
 
 void BFS(int v) {
-	queue<int> q;
+	queue<int> q;	// 방문 큐
+	// 시작 노드 방문
 	q.push(v);
-	visited[v] = 0;	// 방문했으면 0 (DFS에서 쓰던 거 초기화 안 해서 그냥 반전으로...)
+	visited[v] = false;	// 방문: false(DFS 반전)
+
+	// 방문 노드와 연결된 노드들을 큐에 넣고, 순서대로 방문 반복
 	while (!q.empty()) {
+		// q 맨 앞 값을 시작값으로 설정
 		v = q.front();
-		cout << q.front() << ' ';
+		cout << v << " ";
 		q.pop();
-		for (int i = 1; i <= n; i++) {
-			if (visited[i] == 0 || graph[v][i] == 0)
+
+		for (int i = 1; i <= N; i++) {
+			// 간선이 연결되어 있지 않거나 이미 방문한 노드는 건너뛰기
+			if (edges[v][i] == 0 || !visited[i])
 				continue;
+
 			q.push(i);
-			visited[i] = 0;
+			visited[i] = false;
 		}
 	}
 }
 
 int main() {
-	cin >> n >> m >> v;
-	for (int i = 0; i < m; i++) {
+	cin >> N >> M >> V;
+
+	// 간선 입력
+	for (int i = 1; i <= M; i++) {
 		int a = 0, b = 0;
 		cin >> a >> b;
-		graph[a][b] = graph[b][a] = 1;	// 서로 연결되어 있음을 표시 (무방향)
+		// 간선 양방향 연결 (무방향)
+		edges[a][b] = edges[b][a] = 1;
 	}
 
-	DFS(v);
+	DFS(V);
 	cout << "\n";
-	BFS(v);
+	BFS(V);
 
 	return 0;
 }
