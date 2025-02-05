c5890497ace8ab11a10056627671cf291b12c02e
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-02-06 02:21:35 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int k = 0;	// 부등호의 수
+char sign[9] = "";
+
+string minResult = "";
+string maxResult = "";
+
+bool check(char op, int front, int back) {
+	if (op == '<')
+		return front < back;
+	if (op == '>')
+		return front > back;
+	return true;
+}
+
+void DFS_find(int depth, string selectedNum) {
+	/* 자리를 다 채운 경우 최솟값, 최대값 구분하기 */
+	// 오름차순으로 탐색하기 때문에
+	//   minResult가 비어 있으면 현재 발견한 수가 최솟값
+	//   maxResult에 마지막으로 채워진 값이 최대값
+	if (depth == k + 1) {
+		if (minResult == "")
+			minResult = selectedNum;
+		else
+			maxResult = selectedNum;
+		return;
+	}
+
+	for (int i = 0; i <= 9; i++) {
+		// 이미 선택된 문자인 경우에는 뛰어넘기
+		if (selectedNum.find(i + '0') != string::npos) {
+			continue;
+		}
+
+		/* 결과값에 들어갈 숫자 선택하기 */
+		// 선택된 숫자가 없는 경우, 현재 숫자(i) 선택하기
+		// 선택된 숫자가 있는 경우(OR), 부등호를 만족하면 다음 부등호 검사
+		if (depth == 0 || check(sign[depth - 1], selectedNum[depth - 1] - '0', i)) {
+			DFS_find(depth + 1, selectedNum + to_string(i));
+		}
+	}
+
+	return;
+}
+
+int main() {
+	cin >> k;
+
+	for (int i = 0; i < k; i++)
+		cin >> sign[i];
+
+	DFS_find(0, "");
+
+	cout << maxResult << "\n" << minResult;
+
+	return 0;
+}
