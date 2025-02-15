8b34005215cd8f5e15a904c07ffd7c7bf25ec3fb
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-02-16 00:56:07 +0900

    [Silver I] Title: 신입 사원, Time: 460 ms, Memory: 4192 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,41 +1,41 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-
-int T, N;
-int cnt_array[21];
-pair<int,int> score[100000];
 int main() {
-
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    
+	int T = 0, N = 0;
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
+		vector<pair<int, int>> rank;	// first: 서류, second: 면접
 		cin >> N;
 		for (int j = 0; j < N; j++) {
-			cin >> score[j].first >> score[j].second;
+			int a = 0, b = 0;
+			cin >> a >> b;
+			rank.push_back(make_pair(a, b));
 		}
 
-		sort(score, score + N);
+		// 서류 순위(first) 기준으로 정렬
+		sort(rank.begin(), rank.end());
 
-
-		int max_score = score[0].second;
+		// 면접 순위(second) 비교
+		int best = 100001;
 		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			if (score[i].second <= max_score) {
+		for (int j = 0; j < N; j++) {
+			if (rank[j].second < best) {
 				cnt++;
-				max_score = score[i].second;
+				best = rank[j].second;
 			}
 		}
 
-		cnt_array[i] = cnt;
-		
+		// 결과 출력
+		cout << cnt << "\n";
 	}
-
-	for (int i = 0; i < T; i++) {
-		cout << cnt_array[i]<<endl;
-	}
-
 
 	return 0;
-} 
+}
