642277661004cc8ef478f2282a47791d8c07c1d6
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-02-18 21:58:42 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,15 +1,8 @@
 // DP
 // 내 부분수열 크기 = 나보다 작은 거의 부분수열 크기 + 나
 
-// 오답노트: 수열 크기에 마지막 숫자도 포함시켜주기!
-
 #include <iostream>
 using namespace std;
-
-int Max(int a, int b) {
-	if (a > b)	return a;
-	return b;
-}
 
 int main() {
 	int size = 0;	// 수열의 크기
@@ -28,10 +21,10 @@ int main() {
 		dp[i] = 1;
 		for (int j = i - 1; j >= 1; j--) {
 			if (arr[i] > arr[j]) {
-				dp[i] = Max(dp[i], dp[j] + 1);	// 자기자신(i)도 크기에 포함시켜주기
+				dp[i] = max(dp[i], dp[j] + 1);	// 자기자신(i)도 길이에 포함
 			}
 		}
-		answer = Max(dp[i], answer);
+		answer = max(dp[i], answer);
 	}
 
 	cout << answer << endl;
