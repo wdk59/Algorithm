251cfc08d73a01a66a0f7158228a833c6568d8a6
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-02-28 21:57:21 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -5,14 +5,14 @@ int main() {
 	int T = 0;
 	cin >> T;
 	int R = 0;
-	string str = "", result = "";
+	string str = "";
 	for (int i = 0; i < T; i++) {
-		result = "";
 		cin >> R >> str;
 		for (int j = 0; j < str.size(); j++) {
-			result.append(R, str[j]);
+			for (int k = 0; k < R; k++)
+				cout << str[j];
 		}
-		cout << result << '\n';
+		cout << '\n';
 	}
 
 	return 0;
