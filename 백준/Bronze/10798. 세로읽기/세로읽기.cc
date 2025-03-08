86bfecadf272313143709eaee131730d6253feb5
wdk59 <93441225+wdk59@users.noreply.github.com>
2025-03-09 00:26:03 +0900

    Update 세로읽기.cc

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -2,20 +2,14 @@
 using namespace std;
 
 int main() {
-	char characters[5][16] = {};
+	string characters[5];
 
-	int maxSize = 0, cnt = 0;
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++)
 		cin >> characters[i];
-		cnt = 0;
-		while (characters[i][cnt] != '\0')
-			cnt++;
-		maxSize = max(maxSize, cnt);
-	}
 
 	for (int i = 0; i < 15; i++) {
 		for (int j = 0; j < 5; j++) {
-			if (characters[j][i] != '\0')
+			if (characters[j].size() > i)
 				cout << characters[j][i];
 		}
 	}
