d43dbc713af8b044c45c0a45d31276b901d1ad86
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-03-11 01:32:34 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -8,18 +8,13 @@ int main() {
 	cin >> N >> B;
 
 	string result = "";
-	// N을 B로 나눈 나머지가 양수일 때까지 반복
-	while (N >= B) {
+	while (N > 0) {
 		if (N % B < 10)
 			result += to_string(N % B);
 		else
-			result += (N % B - 10 + 'A');	// 10부터 'A'로 표기하기 때문에 10진법에서 10만큼 뺀 다음 'A'의 아스키코드만큼 더해준다.
+			result += (N % B - 10 + 'A');	// 10부터 'A'로 표기
 		N /= B;
 	}
-	if (N < 10)
-		result += to_string(N);
-	else
-		result += (N - 10 + 'A');
 
 	for (int i = result.size() - 1; i >= 0; i--)
 		cout << result[i];
