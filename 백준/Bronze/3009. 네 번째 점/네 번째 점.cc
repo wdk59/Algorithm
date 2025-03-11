6eb67b822d8e50242ef5afc03d969d97431a332e
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-03-12 00:51:14 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -1,25 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
-	vector<int> x, y;
-	int tmpX = 0, tmpY = 0;
+    int x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-	for (int i = 0; i < 3; i++) {
-		cin >> tmpX >> tmpY;
-		x.push_back(tmpX);
-		y.push_back(tmpY);
-	}
+    cout << (x1 ^ x2 ^ x3) << ' ' << (y1 ^ y2 ^ y3) << '\n';
 
-	for (int i = 0; i < 3; i++) {
-		if (count(x.begin(), x.end(), x[i]) == 1)
-			tmpX = x[i];
-		if (count(y.begin(), y.end(), y[i]) == 1)
-			tmpY = y[i];
-	}
-	cout << tmpX << ' ' << tmpY;
-
-	return 0;
+    return 0;
 }
