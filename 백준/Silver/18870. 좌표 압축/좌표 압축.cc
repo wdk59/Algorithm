5d0306243b20db2f8c68fea88c069e04bac8bcc3
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-04-01 19:20:23 +0900

    [Silver II] Title: 좌표 압축, Time: 440 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,27 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
 using namespace std;
 
-int main() {
+int main()
+{
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int N = 0;
+    
+    int N;
     cin >> N;
     vector<int> num(N);
-    for (int i = 0; i < N; i++)
+
+    for (int i = 0; i < N; ++i)
         cin >> num[i];
 
-    set<int> cmpSet(num.begin(), num.end());
-    vector<int> cmpVec(cmpSet.begin(), cmpSet.end());
-    
-    int idx = 0;
-    for (int i = 0; i < N; i++) {
-        idx = lower_bound(cmpVec.begin(), cmpVec.end(), num[i]) - cmpVec.begin();
-        cout << idx << ' ';
-    }
+    vector<int> result = num;
+    sort(result.begin(), result.end());
+    result.erase(unique(result.begin(), result.end()), result.end());
 
-    return 0;
+    for (int val : num)
+        cout << lower_bound(result.begin(), result.end(), val) - result.begin() << ' ';
 }
