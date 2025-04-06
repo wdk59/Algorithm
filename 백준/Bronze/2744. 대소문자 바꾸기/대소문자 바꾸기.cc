1863fdc5e82568aa26bcf310665f879154c088ca
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-04-07 00:02:09 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
 	string str;
 	cin >> str;
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] >= 65 && str[i] <= 90) {
-			str[i] += 32;
-		}
-		else {
-			str[i] -= 32;
-		}
+	for (int i = 0; i < str.size(); i++) {
+		(str[i] >= 65 && str[i] <= 90) ? str[i] += 32 : str[i] -= 32;
 	}
 
 	cout << str;
