35639dfb7c9619a7c8a3905235942fd7c7b075af
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-04-09 02:50:24 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,22 +1,30 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
+
+long long longPow(int base, int exp, long long mod) {
+    long long result = 1;
+    for (int i = exp; i > 0; i--)
+        result = result * base % mod;
+    return result;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int M = 1234567891;
+
     int L = 0;
     cin >> L;
     string str = "";
     cin >> str;
 
-    int result = 0;
+    long long result = 0;
     for (int i = 0; i < L; i++)
-        result += (str[i] - 'a' + 1) * pow(31, i);
+        result += (long long)(str[i] - 'a' + 1) * longPow(31, i, M) % M;
 
-    cout << result;
+    cout << result % M;
 
     return 0;
 }
