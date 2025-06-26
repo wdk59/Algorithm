149427b8b62a5bc276d7ab6eed3a11212d196eda
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-06-26 16:29:33 +0900

    [level 1] Title: 모의고사, Time: 0.31 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -10,18 +10,15 @@ vector<int> solution(vector<int> answers) {
     vector<vector<int>> students = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
     vector<int> scores(3, 0);
     
-    for (int i = 0; i < answers.size(); i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < answers.size(); i++)
+        for (int j = 0; j < 3; j++)
             if (answers[i] == students[j][i % students[j].size()])
                 scores[j]++;
-        }
-    }
     
     int max = *max_element(scores.begin(), scores.end());
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
         if (max == scores[i])
             answer.push_back(i + 1);
-    }
     
     return answer;
 }
