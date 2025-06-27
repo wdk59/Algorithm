a9c45f2be95e90df2778b17db0f13f9dfdae17cc
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-06-28 05:43:42 +0900

    [level 2] Title: 주식가격, Time: 27.42 ms, Memory: 26.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), 0);
+    
+    int begin = 0;
+    for (int i = 0; i < prices.size() - 1; i++) {
+        for (int j = i + 1; j < prices.size(); j++) {
+            answer[i]++;
+            if (prices[i] > prices[j])
+                break;
+        }
+    }
+    
+    
+    return answer;
+}
