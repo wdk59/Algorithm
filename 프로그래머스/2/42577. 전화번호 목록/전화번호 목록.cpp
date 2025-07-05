8642e52eac1d3cf5fea763bee3193d86e7adac76
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-07-05 15:10:27 +0900

    [level 2] Title: 전화번호 목록, Time: 1679.14 ms, Memory: 238 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,26 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-bool isHead(string a, string b) {
-    int i = 0;
-    for (i = 0; i < a.length(); i++) {
-        if (a[i] != b[i])
-            break;
-    }
-    return i == a.length() ? true : false;
-}
-
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    
-    sort(phone_book.begin(), phone_book.end());
-    for (int i = 0; i < phone_book.size() - 1; i++) {
-        if (phone_book[i + 1].substr(0, phone_book[i].size()) == phone_book[i])
-            return false;
+
+    unordered_map<string, int> hash_map;
+    for(int i = 0; i < phone_book.size(); i++)
+        hash_map[phone_book[i]] = 1;
+
+    for(int i = 0; i < phone_book.size(); i++) {
+        string phone_number = "";
+        for(int j = 0; j < phone_book[i].size(); j++) {
+            phone_number += phone_book[i][j];
+            if(hash_map[phone_number] && phone_number != phone_book[i])
+                answer = false;
+        }
     }
-    
+
     return answer;
 }
