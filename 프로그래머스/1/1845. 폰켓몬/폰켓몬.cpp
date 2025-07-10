68e59e588f1e1d2f4a552e817b82fc6229ad14d0
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-07-11 01:11:55 +0900

    [level 1] Title: 폰켓몬, Time: 1.13 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,12 +1,10 @@
 #include <vector>
-#include <unordered_map>
+#include <set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    unordered_map<int, bool> map;
-    for (int i = 0; i < nums.size(); i++)
-        map[nums[i]] = true;
+    set<int> map(nums.begin(), nums.end());
     
     return map.size() < nums.size() / 2 ? map.size() : nums.size() / 2;
 }
