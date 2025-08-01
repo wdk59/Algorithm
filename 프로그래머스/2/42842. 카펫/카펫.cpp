b1b8c5666c8352cd283cc8e73f94199fac90d929
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-01 14:28:44 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,22 +5,15 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     
-    // 약수 구하기
-    vector<int> divisor;
-    for (int i = 1; i <= yellow / 2; i++)
-        if (yellow % i == 0)
-            divisor.push_back(i);
-    divisor.push_back(yellow);
-    
-    // 경우 따지기
-    for (int h: divisor) {
-        int w = yellow / h;
-        if (brown == (h + w) * 2 + 4) {
-            answer.push_back(w + 2);
-            answer.push_back(h + 2);
-            break;
+    for (int i = 1; i <= yellow / 2; i++) {
+        if (yellow % i == 0 && brown == (i + yellow / i) * 2 + 4) {
+            answer.push_back(yellow / i + 2);
+            answer.push_back(i + 2);
+            return answer;
         }
     }
     
+    answer.push_back(3);
+    answer.push_back(yellow + 2);
     return answer;
 }
