cc0eac7a5b13e53555ecf21f89d6bb309d6f9d5e
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-01 14:30:32 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -3,17 +3,10 @@
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
     
-    for (int i = 1; i <= yellow / 2; i++) {
-        if (yellow % i == 0 && brown == (i + yellow / i) * 2 + 4) {
-            answer.push_back(yellow / i + 2);
-            answer.push_back(i + 2);
-            return answer;
-        }
-    }
+    for (int i = 1; i <= yellow / 2; i++)
+        if (yellow % i == 0 && brown == (i + yellow / i) * 2 + 4)
+            return { yellow / i + 2, i + 2 };
     
-    answer.push_back(3);
-    answer.push_back(yellow + 2);
-    return answer;
+    return { 3, yellow + 2 };
 }
