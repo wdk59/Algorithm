857d09edf71552ec8511676041b13060a473e527
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-09 04:54:54 +0900

    [level 4] Title: 단어 퍼즐, Time: 4.05 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -9,26 +8,35 @@ int solution(vector<string> strs, string t)
     int answer = 0;
 
     vector<int> dp(t.size() + 1, INF);
-    dp[0] = 0;
+    dp[0] = 0; // 빈 문자열은 0개의 조각으로 만들 수 있음
+
+    // i: 현재까지 만들어진 문자열의 길이
     for (int i = 0; i < t.size(); i++) {
+        // i번째까지 문자열을 만들 수 없으면 건너뜀
         if (dp[i] == INF)
             continue;
-        
+
+        // 모든 조각 확인
         for (int j = 0; j < strs.size(); j++) {
+            // 범위 확인
             if (i + strs[j].size() > t.size())
                 continue;
-            
+
+            // 일치하는지 직접 한 글자씩 비교 (substr 시간초과 발생)
             bool flag = true;
             for (int k = 0; k < strs[j].size(); k++) {
                 if (t[i + k] != strs[j][k]) {
-                    flag = false;
+                    flag = false; // 하나라도 다르면 실패
                     break;
                 }
             }
+
+            // 일치하는 조각이면, 해당 조각을 붙여서 새 위치의 최소 횟수 갱신
             if (flag)
                 dp[i + strs[j].size()] = min(dp[i + strs[j].size()], dp[i] + 1);
         }
     }
 
+    // 최종적으로 t 전체를 만들 수 없다면 -1 반환, 가능하면 최소 횟수 반환
     return dp[t.size()] == INF ? -1 : dp[t.size()];
 }
