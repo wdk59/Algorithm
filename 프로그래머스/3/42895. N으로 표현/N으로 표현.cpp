3117bf0601dc66694c42d6952c03604acab2a828
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-09 10:42:01 +0900

    [level 3] Title: N으로 표현, Time: 3.79 ms, Memory: 3.76 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -0,0 +1,32 @@
+#include <unordered_set>
+
+using namespace std;
+
+int solution(int N, int number) {
+    unordered_set<int> dp[9];
+    int tmp = N;    // N, NN, NNN, ...
+    for (int i = 1; i < 9; i++) {
+        dp[i].insert(tmp);
+        tmp *= 10;  tmp += N;
+    }
+    
+    for (int i = 2; i < 9; i++) {
+        for (int j = 1; j < i; j++) {
+            for (int left: dp[j]) {
+                for (int right: dp[i - j]) {
+                    dp[i].insert(left + right);
+                    dp[i].insert(left - right);
+                    dp[i].insert(left * right);
+                    if (right != 0)
+                        dp[i].insert(left / right);
+                }
+            }
+        }
+    }
+    
+    for (int i = 1; i < 9; i++)
+        if (dp[i].find(number) != dp[i].end())
+            return i;
+    
+    return -1;
+}
