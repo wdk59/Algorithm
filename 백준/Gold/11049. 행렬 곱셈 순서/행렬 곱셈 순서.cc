1e1e861899a61ba98f4963ef749b96b3b3e1fa2e
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-09 13:33:25 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 32 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -15,18 +14,20 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> mat[i].first >> mat[i].second;
 
-	// DP
+	// DP (dp[i][j]: i번쨰부터 j번째 행렬까지 곱할 때 최소 연산 횟수
 	vector<vector<int>> dp(n, vector<int>(n, 0));
+	// cnt: 곱할 행렬의 개수 - 1
 	for (int cnt = 0; cnt < n; cnt++) {
 		for (int i = 0; i < n - 1 - cnt; i++) {
-			int j = i + cnt + 1;
+			int j = i + cnt + 1;	// 곱할 마지막 행렬의 인덱스
 			dp[i][j] = 2147483647;	// int 최대값
+			// k: 나눠진 기준 행렬 위치
 			for (int k = i; k < j; k++)
-				dp[i][j] = min({ dp[i][j], dp[i][k] + dp[k + 1][j] + mat[i].first * mat[k].second * mat[j].second });
+				dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + mat[i].first * mat[k].second * mat[j].second);
 		}
 	}
 
-	// 출력
+	// 출력: 전체 행렬(0 ~ n-1) 곱한 횟수
 	cout << dp[0][n - 1];
 
 	return 0;
