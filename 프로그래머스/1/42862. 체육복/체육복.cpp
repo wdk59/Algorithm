96747088e1e896a37b17d9b8f42c0bed5ff438d3
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-14 18:50:37 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -12,17 +12,12 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     for (int i = 0; i < reserve.size(); i++)
         cloth[reserve[i] - 1]++;
     
-    for (int i = 0; i < n; i++) {
-        if (cloth[i] == 0) {
-            if (i > 0 && cloth[i - 1] == 2) {
-                cloth[i - 1]--;
-                cloth[i]++;
-            } else if (i < n - 1 && cloth[i + 1] == 2) {
-                cloth[i + 1]--;
-                cloth[i]++;
-            }
-        }
-    }
+    for (int i = 0; i < n; i++)
+        if (cloth[i] == 0)
+            if (i > 0 && cloth[i - 1] == 2)
+                cloth[i - 1] = cloth[i] = 1;
+            else if (i < n - 1 && cloth[i + 1] == 2)
+                cloth[i + 1] = cloth[i] = 1;
     
     for (int i: cloth)
         if (i > 0)
