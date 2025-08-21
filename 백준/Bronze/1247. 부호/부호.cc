63ae916dfe69d877442758a922ac0457a14b6b5e
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-08-21 15:19:54 +0900

    [Bronze III] Title: 부호, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.cc
+++ 백준/Bronze/1247. 부호/부호.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 
 int main() {
@@ -8,25 +7,18 @@ int main() {
 	cout.tie(NULL);
 
 	int t = 3, N = 0, flow = 0;
-	long long sum = 0, tmp = 0;
+	long long tmp = 0;
 	while (t-- > 0) {
+	__int128 sum = 0;
 		sum = 0;
 		flow = 0;
 		cin >> N;
 		while (N-- > 0) {
 			cin >> tmp;
-			if (sum > 0 && tmp > 0 && sum > LLONG_MAX - tmp)
-				flow++;
-			else if (sum < 0 && tmp < 0 && sum < LLONG_MIN - tmp)
-				flow--;
 			sum += tmp;
 		}
 		
-		if (flow > 0)
-			cout << "+\n";
-		else if (flow < 0)
-			cout << "-\n";
-		else if (sum > 0)
+		if (sum > 0)
 			cout << "+\n";
 		else if (sum < 0)
 			cout << "-\n";
