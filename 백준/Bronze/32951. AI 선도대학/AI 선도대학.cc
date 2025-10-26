b2374b89981789065f22a4f51d9969e9c681a767
Seonhyeon Jeon <93441225+wdk59@users.noreply.github.com>
2025-10-27 00:31:14 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int input = 0;
+	cin >> input;
+    cout << input - 2024;
+
+	return 0;
+}
